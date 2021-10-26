74386ffdbb4f4015533971eb3eeb2b07a3e725da
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-26 16:32:58 +0900

    React.jsとTypeScriptをインストール

---
+++ server/resources/ts/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <h1>Laravel SPA</h1>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
